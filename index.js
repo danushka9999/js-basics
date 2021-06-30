4f30ecbc34eb34bebd7ac5f460cb31c46240ede4
danushka9999 <dranasinghe99@gmail.com>
2021-06-30 15:56:06 +0530

    Objects

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 //this is my first java script code
-console.log('Hello  World');
+    console.log('Hello  World');
 
 //Variables
 
-//cannot be reserved keyword
-//should be meaningful
-//they cannot start with a nmuber
-//cannot containg sapce or hyphen(-)
-//use cammel notation
-//case sensitive
+    //cannot be reserved keyword
+    //should be meaningful
+    //they cannot start with a nmuber
+    //cannot containg sapce or hyphen(-)
+    //use cammel notation
+    //case sensitive
 
-let firstName = 'Danushka';
-let lastName = 'Ranasinghe';
-console.log(firstName);
-console.log(lastName);
+    let firstName = 'Danushka';
+    let lastName = 'Ranasinghe';
+    console.log(firstName);
+    console.log(lastName);
 
 //Constants
-const interestRate = 0.3;
-//interestRate = 1;
-//cannot assign a value again because its a constant
-console.log(interestRate);
+    const interestRate = 0.3;
+    //interestRate = 1;
+    //cannot assign a value again because its a constant
+    console.log(interestRate);
 
 //Pirimitive/Value Types
     //String
@@ -27,20 +27,36 @@ console.log(interestRate);
     //Boolean
     //Undefined 
     //Null
-let name = 'Danauhska'; //String literal
-let age = 30; //Number literal
-let isApproved = false; //Bollean literal
-let firstname = 'Undefined';
-let selectColor = null;
-console.log(name);
-console.log(age);
-console.log(isApproved);
-console.log(firstname);
-console.log(selectColor);
+    let name = 'Danauhska'; //String literal
+    let age = 30; //Number literal
+    let isApproved = false; //Bollean literal
+    let firstname = 'Undefined';
+    let selectColor = null;
+    console.log(name);
+    console.log(age);
+    console.log(isApproved);
+    console.log(firstname);
+    console.log(selectColor);
 
 //Dynamic Typing
-//can use 'typeof' to check its pirimitive type
-//undefined is also a value and a type
+    //can use 'typeof' to check its pirimitive type
+    //undefined is also a value and a type
+    //typeof selectColor = object
 
 
+//Refference Types
+    //Object
+    //Array
+    //Fuction
+
+//Object
+    let person = {
+        fname: 'Danu',
+        Age: 21
+    };
+    //Dot Notation(to access property)
+    person.fname = 'Malinga';
+    console.log(person.fname);
+
+    console.log(person);
 
