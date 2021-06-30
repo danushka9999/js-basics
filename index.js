4b15ce52318037aac96a4384dc9f5aad840f3dfa
danushka9999 <dranasinghe99@gmail.com>
2021-06-30 11:23:45 +0530

    Variables

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 //this is my first java script code
 console.log('Hello  World');
+
+let name = 'Danushka';
+console.log(name);
+
+//cannot be reserved keyword
+//should be meaningful
+//they cannot start with a nmuber
+//cannot containg sapce or hyphen(-)
+//use cammel notation
+//case sensitive
+
+let firstName = 'Danushka';
+let lastName = 'Ranasinghe';
+console.log(firstName);
+console.log(lastName);
