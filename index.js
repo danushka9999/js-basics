963b5cd184e1632aa2dc2332ab53aa56be060be1
danushka9999 <dranasinghe99@gmail.com>
2021-07-01 14:15:55 +0530

    Exercise - Stopwatch

--- index.js
+++ index.js
@@ -237,5 +237,41 @@
         });
     }
     const another2 = new Circle2(10);
-    another2.defaultLocation = 0;
+    //another2.defaultLocation = 0;
     another2.draw();
+
+
+//Exercise STOPWATCH
+function Stopwatch() {
+    let startTime, endTime, running, duration = 0; 
+    this.start = function () {
+        if(running)
+            throw new Error('Stopwatch has already started.');
+        running = true;
+        startTime = new Date();
+
+    };
+    this.stop = function () {
+        if(!running)
+            throw new Error('Stopwatch is not started.');
+        running = false;
+        endTime = new Date();
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
+    };
+    this.reset = function () {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+        
+    };
+    Object.defineProperty(this, 'duration', {
+        get: function () {
+            return duration;   
+        }
+    });
+    
+}
+const sw = new Stopwatch();
+
